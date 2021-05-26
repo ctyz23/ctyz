2cd44522164d8b595671dc5a1e5d843fe6249682
ctyz23 <80980481+ctyz23@users.noreply.github.com>
2021-05-26 16:02:30 +0300

    Add files via upload

---
+++ tec/KMSAuto Ne.cmd
@@ -0,0 +1,29 @@
+@ECHO OFF
+color 0B
+mode con:cols=100 lines=25
+echo.
+echo.
+echo.
+@echo    ษอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ           ÜÜÜÜÜÜÜ ÜÜÜÜÜÜÜ ÜÜ   ÜÜ ÜÜÜÜÜÜÜ                               บ
+@echo    บ           Û       Û       Û  Û Û  Û       Û                             บ
+@echo    บ           Û       ÛÜ     ÜÛ  ÛÜÛ  ÛÜÜÜÜ   Û                             บ
+@echo    บ           Û     ÜÜÛ Û   Û Û       ÛÜÜÜÜÛ  Û                             บ
+@echo    บ           Û    Û    Û   Û ÛÜ     ÜÛ ÜÜÜÜÜÜÛ                             บ
+@echo    บ           Û    ÛÜÜ  Û   Û   Û   Û Û ÛÜÜÜÜÜ                              บ
+@echo    บ           ÛÜÜÜÜÜÜÜÛ ÛÜÜÜÛ   ÛÜÜÜÛ ÛÜÜÜÜÜÜÜÛ                             บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ               ฏใแช ฏเฎฃเ ฌฌ๋ KMSAuto Net                               บ
+@echo    บ                                                                         บ
+@echo    ศอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+@echo off
+"%~dp0KMSAuto Net.exe" /S /I
